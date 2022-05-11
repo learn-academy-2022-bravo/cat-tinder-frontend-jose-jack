f42971c6a976e73cc38bcd171e62d07df5150fe2
joagui83 <jjaggie83@gmail.com>
2022-05-11 13:17:06 -0700

    completed trello working on styling

--- src/pages/ReptileIndex.js
+++ src/pages/ReptileIndex.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
-
+import { NavLink } from 'react-router-dom';
 class ReptileIndex extends Component {
+    // constructor(props){
+    //     super(props)
+    
     render() {
-        return (
-            <div>
-                <h1>ReptileIndex</h1>
-            </div>
-        );
+        return(            
+            <>
+            <h1 >Reptile Index</h1>
+                {this.props.reptiles && this.props.reptiles.map(reptile => {
+                return (                                
+                    <NavLink key={reptile.id} to={`/reptileshow/${reptile.id}`}>
+                        {reptile.name}
+                    </NavLink>
+                        )
+                    })}
+                    </>
+            
+        )
     }
+    
 }
 
 export default ReptileIndex;
+
